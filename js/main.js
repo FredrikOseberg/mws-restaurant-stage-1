ebef7fa39401b463e0a57d10aa09972e25e85b57
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-07-18 19:44:12 +0200

    feat: add service worker and ability for service worker to cache visited pages when the user is offline

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var markers = [];
 document.addEventListener('DOMContentLoaded', event => {
   fetchNeighborhoods();
   fetchCuisines();
+  initServiceWorker();
 });
 
 /**
@@ -164,7 +165,7 @@ createRestaurantHTML = restaurant => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
-  more.setAttribute('aria-role', 'button')
+  more.setAttribute('aria-role', 'button');
   more.href = DBHelper.urlForRestaurant(restaurant);
   container.append(more);
 
@@ -185,4 +186,73 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
+};
+
+/**
+ * Handle
+ */
+
+handleUpdateNotificationClick = worker => {
+  worker.postMessage({ action: 'skipWaiting' });
+  document.getElementsByTagName('body')[0].removeChild(document.getElementsByClassName('version-button')[0]);
+};
+
+/**
+ * Notify a user that a newer version is available
+ */
+
+updateNotify = worker => {
+  console.log('running updateNotify');
+  const button = document.createElement('button');
+  button.textContent = 'There is a new version available. Click here to refresh';
+  button.className = 'version-button';
+  button.type = 'button';
+
+  document.getElementsByTagName('body')[0].appendChild(button);
+  document
+    .getElementsByClassName('version-button')[0]
+    .addEventListener('click', () => handleUpdateNotificationClick(worker));
+};
+
+/**
+ * Track installing serviceworker
+ */
+
+trackInstalling = worker => {
+  worker.addEventListener('statechange', () => {
+    if (worker.state === 'installed') {
+      updateNotify(worker);
+      return;
+    }
+  });
+};
+
+/**
+ * Initialize service worker
+ */
+initServiceWorker = () => {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => {
+        console.log('registered', reg);
+        if (!navigator.serviceWorker.controller) return;
+
+        if (reg.waiting) {
+          updateNotify(req.waiting);
+          return;
+        }
+
+        if (reg.installing) {
+          trackInstalling(reg.installing);
+        }
+
+        reg.addEventListener('updatefound', () => trackInstalling(reg.installing));
+      })
+      .catch(err => console.log(err));
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      window.location.reload();
+    });
+  }
 };
---
+++ sw.js
@@ -0,0 +1,56 @@
+const generateId = () => Math.random() * Math.random() * 1;
+
+const staticCacheName = `restaurants-cache-v${generateId()}`;
+
+self.addEventListener('install', event => {
+  const urlsToCache = [
+    '/',
+    '/js/dbhelper.js',
+    '/js/fontawesome-all.min.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyADpmeCuLo0Bkmp3G2ijKBvO3kmiLuljj4&libraries=places&callback=initMap'
+  ];
+
+  event.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(() => console.log('caching failed'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames
+            .filter(cacheName => cacheName.startsWith('restaurants') && cacheName !== staticCacheName)
+            .map(cacheName => caches.delete(cacheName))
+        )
+      )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+      .catch(() => fetch(event.request))
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
